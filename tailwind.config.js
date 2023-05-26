ecc5a27bd80089f60e539c0ebd15bcc9a2c2056e
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-05-25 21:29:33 -0300

    🐳 chore: adiciona paleta

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,32 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        green: {
+          50:'#ECFDF7',
+          100:'#D1FAEC',
+          200:'#A7F3DA',
+          300:'#6EE7BF',
+          400:'#34D39E',
+          500:'#10B981',
+          600:'#059666',
+          700:'#047852',
+          800:'#065F42',
+          900:'#064E36',
+        },
+        purple: {
+          50:'#E2D6FE',
+          100:'#CBB5FD',
+          200:'#c783fd',
+          300:'#8B5CF6',
+          400:'#713AED',
+          500:'#5E28D9',
+          600:'#4E21B6',
+          700:'#421D95',
+          800:'#6e31aa',
+          900:'#13111c',
+        },
+      }
     },
   },
   plugins: [],
