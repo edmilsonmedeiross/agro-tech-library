9c4c3440d098db0f534ced5ced5335d59cddaa71
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-05-29 21:22:55 -0300

    ✨ feat: renderiza sidebar

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import MenuSideBar from '@/components/MenuSideBar';
+
+
+function DashBoard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <aside>
+        <MenuSideBar />
+      </aside>
+    </div>
+  );
+}
+
+export default DashBoard;
