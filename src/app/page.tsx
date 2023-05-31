7e7cde6456dd35f381c971c5a2757c5b6a1f555d
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-05-31 17:27:58 -0300

    ✨ feat: adiciona filtro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import { getAuthors, getLimitedBooks } from '@/actions';
 import CarouselComponent from '@/components/CarouselComponent';
+import FilterBooksPerAuthor from '@/components/FilterBooksPerAuthor';
 import PaginationComponent from '@/components/PaginationComponent';
 import React from 'react';
 
 export default async function Home(): Promise<React.JSX.Element> {
+  const [authors, carouselBooks] = await Promise.all([getAuthors(), getLimitedBooks(10)]);
   
   return (
-    <div className="flex flex-col bg-purple-900 w-screen min-h-screen">
-      <h1>Hello, Agro Tech!</h1>
-      <CarouselComponent />
+    <div className="flex flex-col bg-green-600 min-h-screen">
+      <h1>Agro Tech Library!</h1>
+      <FilterBooksPerAuthor authors={ authors } />
+      <CarouselComponent renderBooks={ carouselBooks } />
       <PaginationComponent />
     </div>
   );
