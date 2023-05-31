4a6d4b3a16346580ec4d66f3ec1c355bbd2297eb
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-05-31 10:52:35 -0300

    🎉 init: cria page dinamica

---
+++ src/app/dashboard/books/edit/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { getAuthors, getBookById, getCategories } from '@/actions';
+import SearchBook from '@/components/SearchBook';
+import React from 'react';
+
+async function EditBookId({ params }: { params: { id: string }}) {
+  const [book, authors, categoriesOpitions] = await Promise.all([
+    getBookById(Number(params.id)),
+    getAuthors(),
+    getCategories(),
+  ]);
+
+  return (
+    <div>
+      <SearchBook
+        book={ book }
+        authors={ authors }
+        categoriesOpitions={ categoriesOpitions }
+        context="edit"
+      />
+    </div>
+  );
+}
+
+export default EditBookId;
