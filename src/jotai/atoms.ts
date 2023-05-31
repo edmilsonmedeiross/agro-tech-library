bfe6a37013607e3417631a6ca821887885ccf19e
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-05-31 17:30:29 -0300

    ✨ feat: cria atomo global

--- src/jotai/atoms.ts
+++ src/jotai/atoms.ts
@@ -1,7 +1,22 @@
 // import { BookCardProps } from '@/types/Book';
 import { type BookCardProps } from '@/components/BookCard';
+import { AuthorProps } from '@/types/Author';
 import { atom } from 'jotai';
 
 export const isVisibleAtom = atom<boolean>(false);
 export const booksForRenderAtom = atom<BookCardProps[]>([]);
+export const totalBooksAtom = atom(0);
 export const isVisibleEditModalAtom = atom<boolean>(false);
+export const pageAtom = atom<number>(1);
+
+//registerBooksAtom
+export const authorOptionsAtom = atom<AuthorProps[]>([]);
+export const isSubmitingAtom = atom(false);
+export const openModalAtom = atom(false);
+
+// searchBooksAtom
+
+export const bookForEditAtom = atom<BookCardProps | undefined>(undefined);
+
+// authorFormAtom
+export const authorExistsAtom = atom(false);
