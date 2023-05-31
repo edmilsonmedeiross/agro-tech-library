b5d2146a6ad8331901b06a17ef9cecb41ba91e9f
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-05-31 10:47:18 -0300

    ♻️ Refactorr: refatora

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default async function Home(): Promise<React.JSX.Element> {
   
   return (
-    <div className="flex flex-col justify-center w-screen h-screen bg-purple-900">
+    <div className="flex flex-col bg-purple-900 w-screen min-h-screen">
       <h1>Hello, Agro Tech!</h1>
       <CarouselComponent />
       <PaginationComponent />
