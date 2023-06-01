3ebb6c3ddff62eea072c26a4e3ad24bcbcf6faab
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-05-31 22:15:10 -0300

    ♻️ Refactorr: refatora

--- src/components/PaginationComponent.tsx
+++ src/components/PaginationComponent.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { useAtom } from 'jotai';
 import { booksForRenderAtom, pageAtom, totalBooksAtom } from '@/jotai/atoms';
